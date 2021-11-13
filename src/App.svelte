71fd491730eaa836b879a0abe81457d787f4bcb8
Wolf McNally <wolf@wolfmcnally.com>
2021-11-13 00:16:07 -0800

    First commit.

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import CanvasTest from './lib/CanvasTest.svelte'
+</script>
+
+<main>
+  <CanvasTest />
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/lib/CanvasTest.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import * as createjs from 'createjs-module';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    var stage = new createjs.Stage("demoCanvas");
+    var circle = new createjs.Shape();
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+    circle.x = 10;
+    circle.y = 10;
+    stage.addChild(circle);
+
+    stage.update();
+
+    createjs.Tween.get(circle, { loop: true })
+    .to({ x: 400 }, 1000, createjs.Ease.getPowInOut(4))
+    .to({ alpha: 0, y: 175 }, 500, createjs.Ease.getPowInOut(2))
+    .to({ alpha: 0, y: 225 }, 100)
+    .to({ alpha: 1, y: 200 }, 500, createjs.Ease.getPowInOut(2))
+    .to({ x: 100 }, 800, createjs.Ease.getPowInOut(2));
+
+    createjs.Ticker.framerate = 60;
+    createjs.Ticker.addEventListener("tick", stage);
+  });
+</script>
+
+<canvas id="demoCanvas" width="500" height="500"></canvas>
